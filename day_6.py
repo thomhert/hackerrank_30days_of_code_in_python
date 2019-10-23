fac8685fc12614307ae07d6272479bfbc810f2fe
Thomhert Siadari <thomhert@ust.ac.kr>
2019-10-23 15:44:16 +0900

    add day_7

---
+++ day_6.py
@@ -0,0 +1,23 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+def printEven(s):
+    output = ""
+    for i in range(len(s)):
+        if i % 2 == 0:
+            output += s[i]
+
+    return output
+
+
+def printOdd(s):
+    output = ""
+    for i in range(len(s)):
+        if i % 2 != 0:
+            output += s[i]
+
+    return output
+
+t = int(input())
+
+for i in range(0, t):
+    s = input()
+    print(printEven(s) + " " + printOdd(s))
